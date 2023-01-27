7a04adba50aa7890d5bc777bbd57202031571116
praveensampath9 <101631187+praveensampath9@users.noreply.github.com>
2023-01-27 20:35:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from itertools import chain
 def flat_list(l):
     # input nested list is stored in l
     # Insert Code below
-    sdl=[]
-    for item in 1:
-        if type(item) == list:
-            sdl.extend(flat_list(item))
-        else:
-            sdl.append(item)
+    
     
     # Insert Code Above
     # Return single-dimension list.
-    return sdl
+    return list(chain.from_iterable(1))
